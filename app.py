c41cae7517906ee18233b3b4d3b8dfe6f4026d96
Hue Kim <opklnm102@gmail.com>
2021-04-05 17:10:56 +0900

    Add mysql

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+
+import mysql.connector
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +9,60 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/widgets')
+def get_widgets():
+    mydb = mysql.connector.connect(
+        host="mysql-local",
+        port=3306,
+        user="root",
+        password="p@ssword1",
+        database="inventory"
+    )
+
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM widgets")
+
+    row_headers = [x[0] for x in cursor.description]
+    results = cursor.fetchall()
+    json_data = []
+    for result in results:
+        json_data.append(dict(zip(row_headers, result)))
+
+    cursor.close()
+
+    return json.dumps(json_data)
+
+
+@app.route('/initdb')
+def db_init():
+    mydb = mysql.connector.connect(
+        host="mysql-local",
+        port=3306,
+        user="root",
+        password="p@ssword1"
+    )
+
+    cursor = mydb.cursor()
+    cursor.execute("DROP DATABASE IF EXISTS inventory")
+    cursor.execute("CREATE DATABASE inventory")
+    cursor.close()
+
+    mydb = mysql.connector.connect(
+        host="mysql-local",
+        port=3306,
+        user="root",
+        password="p@ssword1",
+        database="inventory"
+    )
+
+    cursor = mydb.cursor()
+    cursor.execute("DROP TABLE IF EXISTS widgets")
+    cursor.execute("CREATE TABLE widgets (name VARCHAR (255), description VARCHAR(255))")
+    cursor.close()
+
+    return 'init database'
 
 
 if __name__ == '__main__':
